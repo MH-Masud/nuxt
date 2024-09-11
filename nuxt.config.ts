e667b778e1d84627150a4253943a277c3c3cec42
MH Masud <44880598+MH-Masud@users.noreply.github.com>
2024-09-11 08:24:30 +0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  extends: [
-    '@nuxt/examples-ui'
-  ],
   app: {
     head: {
       charset: 'utf-8',
